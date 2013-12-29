cad30d2481ed202a0171e61cd13afb21495f6668
Linux User <sudormdashrfslash@gmail.com>
2013-12-28 21:13:30 -0800

    Add percentage marks

--- tamarin.py
+++ tamarin.py
@@ -5,15 +5,19 @@ CD = os.path.dirname(os.path.realpath(__file__))
 INPUT = os.path.join(CD, "input")
 OUTPUT = os.path.join(CD, "output")
 max_dist = 5
+pixels_to_modify = 10 
 times = 1 
 
 def tamarin():
     images = os.listdir(INPUT) 
+    done = 0
+    total = times*len(images)
     for n in range(times):
         for image in images:
             output(image)
             mutate(image)
-        print n
+            done += 1
+            print "%d%% done." % (float(done)/float(total)*100.0)
 
 def output(image):
     realpath = os.path.join(INPUT, image)
@@ -22,15 +26,17 @@ def output(image):
 def mutate(path):
     realpath = os.path.join(INPUT, path)
     image = Image.open(realpath)
-    data = image.getdata()
+    data = list(image.getdata())
     size = image.size
 
-    for pos in data:
-        n = random.choice(get_neigbors(pos, size[0], len(data)))
+    for _ in range(pixels_to_modify):
+        pos = random.randint(0, len(data))
+        n = random.choice(get_neighbors(pos, size[0], len(data)))
         swap(n, pos, data)
+        print "pos is %d" % pos
 
     image.putdata(data)
-    Image.save(realpath)
+    image.save(realpath)
         
 def swap(n, pos, data):
     data[n], data[pos] = data[pos], data[n]
@@ -41,7 +47,7 @@ def get_neighbors(a, width, total_len):
 
     works = []
 
-    for b in range(len(size)):
+    for b in range(total_len):
          arow, brow = int(a / width), int(b / width)
          acol, bcol = a % width, b % width
          if abs(acol - bcol) + abs(arow - brow) <= max_dist:
@@ -50,5 +56,4 @@ def get_neighbors(a, width, total_len):
     return works
     
     
-      
-
+tamarin()
