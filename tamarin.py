dff2bac6e0aa2fdd6f10ca65e7bac7114a3e9203
Linux User <sudormdashrfslash@gmail.com>
2013-12-28 20:56:58 -0800

    Add beautiful, efficient get_neighbor code

--- tamarin.py
+++ tamarin.py
@@ -5,7 +5,7 @@ CD = os.path.dirname(os.path.realpath(__file__))
 INPUT = os.path.join(CD, "input")
 OUTPUT = os.path.join(CD, "output")
 max_dist = 5
-times = 5
+times = 1 
 
 def tamarin():
     images = os.listdir(INPUT) 
@@ -23,9 +23,10 @@ def mutate(path):
     realpath = os.path.join(INPUT, path)
     image = Image.open(realpath)
     data = image.getdata()
+    size = image.size
 
     for pos in data:
-        n = random.choice(get_neigbors(pos, len(data)))
+        n = random.choice(get_neigbors(pos, size[0], len(data)))
         swap(n, pos, data)
 
     image.putdata(data)
@@ -35,7 +36,19 @@ def swap(n, pos, data):
     data[n], data[pos] = data[pos], data[n]
     return data
 
-def get_neighbors(pos, size):
-    pass
+def get_neighbors(a, width, total_len):
+    # Uses "night's move" geometry for speed
+
+    works = []
+
+    for b in range(len(size)):
+         arow, brow = int(a / width), int(b / width)
+         acol, bcol = a % width, b % width
+         if abs(acol - bcol) + abs(arow - brow) <= max_dist:
+             works.append(b)
+
+    return works
+    
+    
       
 
